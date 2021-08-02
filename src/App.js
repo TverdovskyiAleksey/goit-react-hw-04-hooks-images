df2e1482704418543beb4572973777f7bbc9fa2d
Alex <tverdovskyi92@gmail.com>
2021-08-02 11:51:37 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import SearchBar from './Components/SearchBar';
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ImageGallery from './Components/ImageGallery';
 import Modal from './Modal';
 import Container from './Components/Container';
@@ -7,50 +7,51 @@ import Button from './Components/Button';
 import MyLoader from './Components/Loader/Loader';
 import Api from './Services/Api';
 
-export default class App extends Component {
-  state = {
-    hits: [],
-    currentPage: 1,
-    searchQuery: '',
-    isLoading: false,
-    showModal: false,
-    error: null,
-    url: '',
-    tag: '',
+export default function App() {
+  const [hits, setHits] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [url, setUrl] = useState('');
+  const [tag, setTag] = useState('');
+
+  useEffect(() => {
+    fetchhits();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchQuery]);
+
+  const onChangeQuery = query => {
+    setSearchQuery(query);
+    setCurrentPage(1);
+    setHits([]);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.fetchhits();
+  const handleImageClick = ({ target }) => {
+    if (target.nodeName !== 'IMG') {
+      return;
     }
-  }
-
-  onChangeQuery = query => {
-    this.setState({
-      searchQuery: query,
-      currentPage: 1,
-      hits: [],
-      error: null,
-    });
+    const { url } = target.dataset;
+    const tag = target.alt;
+    setUrl(url);
+    setTag(tag);
+    setIsLoading(false);
+    toggleModal(false);
   };
 
-  fetchhits = () => {
-    const { currentPage, searchQuery } = this.state;
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
 
-    this.setState({ isLoading: true });
+  const fetchhits = () => {
+    setIsLoading(true);
+    const options = { searchQuery, currentPage };
 
-    return Api.findImage(currentPage, searchQuery)
-      .then(hits => {
-        this.setState(prevState => ({
-          hits: [...prevState.hits, ...hits],
-          currentPage: prevState.currentPage + 1,
-        }));
+    Api.findImage(options)
+      .then(responseHits => {
+        setHits(prevHits => [...prevHits, ...responseHits]);
+        setCurrentPage(prevCurrentpage => prevCurrentpage + 1);
       })
       .then(() => {
         window.scrollTo({
@@ -58,39 +59,109 @@ export default class App extends Component {
           behavior: 'smooth',
         });
       })
-      .catch(error => this.setState({ error }))
-      .finally(() => this.setState({ isLoading: false }));
+      .catch(error => setError({ error }))
+      .finally(() => setIsLoading(false));
   };
 
-  handleImageClick = ({ target }) => {
-    if (target.nodeName !== 'IMG') {
-      return;
-    }
-    const { url } = target.dataset;
-    const tag = target.alt;
-    this.setState({
-      url,
-      tag,
-      isLoading: false,
-    });
-    this.toggleModal();
-  };
-
-  render() {
-    const { hits, showModal, isLoading, url, tag } = this.state;
-
-    return (
-      <Container>
-        <SearchBar onSubmit={this.onChangeQuery} />
-        <ImageGallery hits={hits} onClick={this.handleImageClick} />
-        {isLoading && <MyLoader />}
-        {hits.length > 0 && !isLoading && <Button onClick={this.fetchhits} />}
-        {showModal && (
-          <Modal onClose={this.toggleModal} onClick={this.handleImageClick}>
-            <img src={url} alt={tag} />
-          </Modal>
-        )}
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <SearchBar onSubmit={onChangeQuery} />
+      <ImageGallery hits={hits} onClick={handleImageClick} />
+      {isLoading && <MyLoader />}
+      {hits.length > 0 && !isLoading && <Button onClick={fetchhits} />}
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={url} alt={tag} />
+        </Modal>
+      )}
+    </Container>
+  );
 }
+
+// export default class App extends Component {
+//   state = {
+//     hits: [],
+//     currentPage: 1,
+//     searchQuery: '',
+//     isLoading: false,
+//     showModal: false,
+//     error: null,
+//     url: '',
+//     tag: '',
+//   };
+
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal,
+//     }));
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.searchQuery !== this.state.searchQuery) {
+//       this.fetchhits();
+//     }
+//   }
+
+// onChangeQuery = query => {
+//   this.setState({
+//     searchQuery: query,
+//     currentPage: 1,
+//     hits: [],
+//     error: null,
+//   });
+// };
+
+// fetchhits = () => {
+//   const { currentPage, searchQuery } = this.state;
+
+//   this.setState({ isLoading: true });
+
+//   return Api.findImage(currentPage, searchQuery)
+//     .then(hits => {
+//       this.setState(prevState => ({
+//         hits: [...prevState.hits, ...hits],
+//         currentPage: prevState.currentPage + 1,
+//       }));
+//     })
+//     .then(() => {
+//       window.scrollTo({
+//         top: document.documentElement.scrollHeight,
+//         behavior: 'smooth',
+//       });
+//     })
+//     .catch(error => this.setState({ error }))
+//     .finally(() => this.setState({ isLoading: false }));
+// };
+
+// handleImageClick = ({ target }) => {
+//   if (target.nodeName !== 'IMG') {
+//     return;
+//   }
+//   const { url } = target.dataset;
+//   const tag = target.alt;
+//   this.setState({
+//     url,
+//     tag,
+//     isLoading: false,
+//   });
+//   this.toggleModal();
+// };
+
+//   render() {
+//     const { hits, showModal, isLoading, url, tag } = this.state;
+
+//     return (
+// <Container>
+//   <SearchBar onSubmit={this.onChangeQuery} />
+//   <ImageGallery hits={hits} onClick={this.handleImageClick} />
+//   {isLoading && <MyLoader />}
+//   {hits.length > 0 && !isLoading && <Button onClick={this.fetchhits} />}
+//   {showModal && (
+//     <Modal onClose={this.toggleModal} onClick={this.handleImageClick}>
+//       <img src={url} alt={tag} />
+//     </Modal>
+//   )}
+// </Container>
+//     );
+//   }
+// }
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,39 +1,31 @@
 import styles from './Modal.module.css';
-import React, { Component } from 'react';
-import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const modalRoot = document.querySelector('#modal-root');
-
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
+export default function Modal({ onClose, children }) {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackDropClick = e => {
+  const handleBackDropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={styles.Overlay} onClick={this.handleBackDropClick}>
-        <div className={styles.Modal}>{this.props.children}</div>
-      </div>,
-      modalRoot,
-    );
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className={styles.Overlay} onClick={handleBackDropClick}>
+      <div className={styles.Modal}>{children}</div>
+    </div>
+  );
 }
 
 Modal.propTypes = {
